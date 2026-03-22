3cdbc483be206d653171a4cebaea59a2ab929b1b
Gloria Oteri <gloriaoteri@gmail.com>
2026-03-22 16:12:11 -0400

    Updated titles in chapter data file

--- lib/data/chapter_data.dart
+++ lib/data/chapter_data.dart
@@ -3,26 +3,26 @@ final List storyChapters = [
 
   Chapter(
     id: 01,
-    title: "Chapter 1",
+    title: "Chapter 1 - The Gate",
     description: ""),
   
   Chapter(
     id: 02,
-    title: "Chapter 2",
+    title: "Chapter 2 - Into the Rose Garden",
     description: ""),
   
   Chapter(
     id: 03,
-    title: "Chapter 3",
+    title: "Chapter 3 - A Bloody Entrance",
     description: ""),
   
   Chapter(
     id: 04,
-    title: "Chapter 4",
+    title: "Chapter 4 - The Haunted Mansion",
     description: ""),
   
   Chapter(
     id: 05,
-    title: "Chapter 5",
+    title: "Chapter 5 - The Bedroom",
     description: ""),
 ];
