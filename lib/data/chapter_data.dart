c5cb7544cbf086ea175336703a06bb0ed6846acb
Gloria Oteri <gloriaoteri@gmail.com>
2026-03-22 14:02:04 -0400

    Update chapter data file with chapters ids and titles

---
+++ lib/data/chapter_data.dart
@@ -0,0 +1,28 @@
+import '../models/chapter.dart';
+final List storyChapters = [
+
+  Chapter(
+    id: 01,
+    title: "Chapter 1",
+    description: ""),
+  
+  Chapter(
+    id: 02,
+    title: "Chapter 2",
+    description: ""),
+  
+  Chapter(
+    id: 03,
+    title: "Chapter 3",
+    description: ""),
+  
+  Chapter(
+    id: 04,
+    title: "Chapter 4",
+    description: ""),
+  
+  Chapter(
+    id: 05,
+    title: "Chapter 5",
+    description: ""),
+];
