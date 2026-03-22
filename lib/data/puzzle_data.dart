0bb94a3a4885a397453cc320ed53fe4e151c26e5
Gloria Oteri <gloriaoteri@gmail.com>
2026-03-22 17:40:46 -0400

    Added puzzle ids and chapter ids

---
+++ lib/data/puzzle_data.dart
@@ -0,0 +1,43 @@
+import "../models/puzzle.dart";
+
+final List storyPuzzles = [
+  Puzzle(
+    id: 001,
+    chapterId: 01,
+    question: "",
+    answer: "",
+    hint: ""
+  ),
+
+  Puzzle(
+    id: 002,
+    chapterId: 02,
+    question: "",
+    answer: "",
+    hint: ""
+  ),
+
+  Puzzle(
+    id: 003,
+    chapterId: 03,
+    question: "",
+    answer: "",
+    hint: ""
+  ),
+
+  Puzzle(
+    id: 004,
+    chapterId: 04,
+    question: "",
+    answer: "",
+    hint: ""
+  ),
+
+  Puzzle(
+    id: 005,
+    chapterId: 05,
+    question: "",
+    answer: "",
+    hint: ""
+  ),
+];
