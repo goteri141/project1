b7fb306ffa448b664715975b2f80947b342d6030
Gloria Oteri <gloriaoteri@gmail.com>
2026-03-22 17:16:20 -0400

    Updated chapter descriptions in chapter data file

--- lib/data/chapter_data.dart
+++ lib/data/chapter_data.dart
@@ -4,25 +4,25 @@ final List storyChapters = [
   Chapter(
     id: 01,
     title: "Chapter 1 - The Gate",
-    description: ""),
+    description: "Welcome to the Mystery Puzzle & Escape Room Companion.\nYou will step into the role of a detective.\nThe victim, Sarah Thompson, a 34-year-old woman, was found dead inside her mansion with no signs of forced entry.\nYou arrived at Ms. Thompson's mansion.\nBut something stands in your way.\nThe iron gate is locked.\nSolve the riddle to gain access to the mansion."),
   
   Chapter(
     id: 02,
     title: "Chapter 2 - Into the Rose Garden",
-    description: ""),
+    description: "The iron gate slowly opens.\nYou step inside and walked along the paved walkway.\n You stumbled upon the rose garden.\nThe path is narrow, surrounded by towering bushes of thorns and blooms.\nYou've noticed something strange.\nHalf of the roses in the garden are darker, their petails slightly wilted like someone intentionally did it.\nSolve the riddle to uncover the clue in the garden."),
   
   Chapter(
     id: 03,
     title: "Chapter 3 - A Bloody Entrance",
-    description: ""),
+    description: "You approach the front door of the mansion.\nAs you attempt to open the door, a dark, handprint stain marks the handle... blood.\nBut there are no signs of struggle nearby. No broken glass or forced entry.\nSomething about this doesn't add up. \nYou open the door.\nBefore you stepped inside, you noticed a clue.\nSolve the riddle to unciver what happened at the entrance."),
   
   Chapter(
     id: 04,
     title: "Chapter 4 - The Haunted Mansion",
-    description: ""),
+    description: "The door shuts behind you as you make your way inside.\nFurniture nealty arranged, as if nothing ever happened.\nPortraits line the walls, giviing off a creepy and eerie aura.\nHowever, you've noticed one frame is missing.\nSolve the riddle to uncover the missing portrait."),
   
   Chapter(
     id: 05,
     title: "Chapter 5 - The Bedroom",
-    description: ""),
+    description: "You walked upstairs and made your way to the bedroom at the beginning of the hall.\nThis is where Sarah Thompson was found.\nThe room is dark, you turned on your flashlight and found a diary opened on the nightstand.\nYou look through the pages, but one entry stands out.\nIt felt rushed like she knew something. Maybe even the person responsible for her death.\nSolve the riddle to uncover the truth in her message.   "),
 ];
